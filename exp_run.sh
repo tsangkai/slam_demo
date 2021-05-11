5b7b89dffca479dec6abf2ae9c629436499182da
tsangkai <tsangkai.chang@gmail.com>
2021-05-11 11:03:12 -0700

    change apps -> exp

--- exp_run.sh
+++ exp_run.sh
@@ -5,24 +5,24 @@ dataset_list=( MH_01 MH_02 MH_03 MH_04 MH_05 )
 
 for dataset in ${dataset_list[*]} 
 do
-  # ./build/apps/backend_opt_app ${dataset}
+  # ./build/exp/backend_opt_app ${dataset}
   # read -p "press key"
-  ./build/apps/backend_em_app ${dataset}
+  ./build/exp/backend_em_app ${dataset}
   # read -p "press key"
-  # ./build/apps/backend_boem_app ${dataset}
+  # ./build/exp/backend_boem_app ${dataset}
   # read -p "press key"
 done
 
 
 for dataset in ${dataset_list[*]} 
 do
-  # ./build/apps/traj_process_app ${dataset} traj_opt
-  ./build/apps/traj_process_app ${dataset} traj_em
-  # ./build/apps/traj_process_app ${dataset} traj_boem
+  # ./build/exp/traj_process_app ${dataset} traj_opt
+  ./build/exp/traj_process_app ${dataset} traj_em
+  # ./build/exp/traj_process_app ${dataset} traj_boem
 done
 
 
 for dataset in ${dataset_list[*]} 
 do
-  python3 eval_exp.py ${dataset}
+  python3 exp_eval.py ${dataset}
 done
