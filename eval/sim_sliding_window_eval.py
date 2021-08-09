f47aab8628fc0f79b4ea7370513fab3bd321f75a
tsangkai <tsangkai.chang@gmail.com>
2021-08-09 15:38:12 -0700

    update sim_sliding_window

--- eval/sim_sliding_window_eval.py
+++ eval/sim_sliding_window_eval.py
@@ -11,17 +11,19 @@ import visualization
 fig_width = 5.84
 fig_height = 4.38
 
+algorithms = ['opt', 'em', 'boem']
+algorithms_display = ['opt.', 'EM', 'BOEM']
+
 # error plot
 num_realizations = int(sys.argv[1])
 gt_data = pd.read_csv("result/sim/sliding_window/gt.csv")
-result_string = ['opt', 'em', 'boem']
-p_error_array = np.zeros((len(result_string), len(gt_data['p_x']), num_realizations))
+p_error_array = np.zeros((len(algorithms), len(gt_data['p_x']), num_realizations))
 q_error_array = np.zeros_like(p_error_array)
 
 
-for l in range(len(result_string)):
+for l in range(len(algorithms)):
 	for k in range(num_realizations):
-		data = pd.read_csv("result/sim/sliding_window/%s_%s.csv" % (result_string[l],k))
+		data = pd.read_csv("result/sim/sliding_window/%s_%s.csv" % (algorithms[l],k))
 		for i in range(len(gt_data['p_x'])):
 			p_error_array[l, i, k] = math.sqrt( (gt_data['p_x'][i]-data['p_x'][i])**2 + (gt_data['p_y'][i]-data['p_y'][i])**2
 												 + (gt_data['p_z'][i]-data['p_z'][i])**2)
@@ -32,27 +34,29 @@ for l in range(len(result_string)):
 fig, (ax1, ax2) = plt.subplots(2)
 fig.set_size_inches(fig_width, fig_height)
 line_width = 1.2
-for n in range(len(result_string)):
+for n in range(len(algorithms)):
 	error_q_m =  np.mean(q_error_array[n], axis=1)
 	error_q_s = np.std(q_error_array[n], axis=1)
 
 	ax1.fill_between(gt_data['timestamp'], error_q_m - error_q_s/2,
-					 error_q_m + error_q_s/2, color=visualization.color[result_string[n]], alpha=0.5, linewidth=0.0)
+					 error_q_m + error_q_s/2, color=visualization.color[algorithms[n]], alpha=0.5, linewidth=0.0)
 
 	error_p_m =  np.mean(p_error_array[n], axis=1)
 	error_p_s = np.std(p_error_array[n], axis=1)
 	ax2.fill_between(gt_data['timestamp'], error_p_m - error_p_s/2,
-					 error_p_m + error_p_s/2, color=visualization.color[result_string[n]], alpha=0.5, linewidth=0.0)
+					 error_p_m + error_p_s/2, color=visualization.color[algorithms[n]], alpha=0.5, linewidth=0.0)
 
-	ax1.plot(gt_data['timestamp'], error_q_m, color=visualization.color[result_string[n]], linewidth=line_width, label=result_string[n])
-	ax2.plot(gt_data['timestamp'], error_p_m, color=visualization.color[result_string[n]], linewidth=line_width, label=result_string[n])
+	ax1.plot(gt_data['timestamp'], error_q_m, color=visualization.color[algorithms[n]], linewidth=line_width, label=algorithms_display[n])
+	ax2.plot(gt_data['timestamp'], error_p_m, color=visualization.color[algorithms[n]], linewidth=line_width, label=algorithms_display[n])
 
 
 ax1.set(ylabel='rotation RMSE [deg]')
 ax1.set_ylim(visualization.add_boarder((0, 3)))
+ax1.legend(loc = 1)
+
 ax2.set(ylabel='position RMSE [m]')
 ax2.set(xlabel='time [s]')
-ax2.legend(loc = 1)
 ax2.set_ylim(visualization.add_boarder((0, 0.4)))
+
 plt.show()
 
--- sim_sliding_window_run.sh
+++ sim_sliding_window_run.sh
@@ -2,11 +2,11 @@
 
 DURATION="1000"  #300
 TRIALS="10"   #50
-FIXED_WINDOW_SIZE="60"
+FIXED_WINDOW_SIZE="100"
 
 ./bin/sim_sliding_window_app      -duration=$DURATION
-./bin/sim_sliding_window_opt_app  -duration=$DURATION -trials=$TRIALS
+./bin/sim_sliding_window_opt_app  -duration=$DURATION -trials=$TRIALS -fixed_window_size=$FIXED_WINDOW_SIZE
 ./bin/sim_sliding_window_em_app   -duration=$DURATION -trials=$TRIALS -fixed_window_size=$FIXED_WINDOW_SIZE
-./bin/sim_sliding_window_boem_app -duration=$DURATION -trials=$TRIALS -fixed_window_size=$FIXED_WINDOW_SIZE
+./bin/sim_sliding_window_boem_app -duration=$DURATION -trials=$TRIALS
 
 python eval/sim_sliding_window_eval.py $TRIALS
